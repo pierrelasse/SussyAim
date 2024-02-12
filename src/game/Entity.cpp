4b1bcf8cc6a84c6ddb3f79a9f7dff928fbb57410
pierrelasse <pierrelassebusiness@gmail.com>
2024-02-12 17:25:15 +0100

    refactor(PlayerPawn::GetWeaponName): change buffer from 525 to 50 for some reason

--- src/game/Entity.cpp
+++ src/game/Entity.cpp
@@ -127,13 +127,13 @@ bool PlayerPawn::GetSpotted()
 bool PlayerPawn::GetWeaponName()
 {
 	DWORD64 WeaponNameAddress = 0;
-	char buffer[525]{};
+	char buffer[50]{};
 
 	WeaponNameAddress = ProcessMgr.TraceAddress(this->Address + Offset::Pawn.pClippingWeapon, {0x10, 0x20, 0x0});
 	if (WeaponNameAddress == 0)
 		return false;
 
-	if (!ProcessMgr.ReadMemory(WeaponNameAddress, buffer, 525))
+	if (!ProcessMgr.ReadMemory(WeaponNameAddress, buffer, 50))
 		return false;
 
 	WeaponName = std::string(buffer);
