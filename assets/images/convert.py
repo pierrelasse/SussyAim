03cd21c33989807cd0522f4bc01ed987b1d11912
pierrelasse <pierrelassebusiness@gmail.com>
2024-03-13 12:39:25 +0100

    feat: improve convert.py

--- assets/images/convert.py
+++ assets/images/convert.py
@@ -1,8 +1,25 @@
+import os
 import binascii
 
-def imgToArr(path):
-    with open(path, 'rb') as image_file:
-        hex_data = binascii.hexlify(image_file.read()).decode('utf-8')
-        print(f"{{\n    {', '.join([f'0x{hex_data[i:i+2]}' for i in range(0, len(hex_data), 2)])}\n}}")
+path = input("Path: ")
 
-imgToArr(input("Path: "))
+if not os.path.isfile(path):
+    print("The file does not exist!")
+    input()
+    exit()
+
+hex_data: str = None
+
+with open(path, "rb") as f:
+    hex_data = binascii.hexlify(f.read()).decode("utf-8")
+    
+if hex_data == None:
+    print("Could not hexlify")
+    input()
+    exit()
+
+with open(path + ".txt", "w") as f:
+    f.write(f"{{\n    {', '.join([f'0x{hex_data[i:i+2]}' for i in range(0, len(hex_data), 2)])}\n}}")
+    print("Success!")
+        
+input()
